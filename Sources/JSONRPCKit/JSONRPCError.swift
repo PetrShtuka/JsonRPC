73e32992d3f9aa6638a10d02c76a921f681d27b1
Petr Shtuka <48615436+PetrShtuka@users.noreply.github.com>
2023-10-07 10:13:36 +0300

    Update JSONRPCError.swift

--- Sources/JSONRPCKit/JSONRPCError.swift
+++ Sources/JSONRPCKit/JSONRPCError.swift
@@ -2,67 +2,8 @@
 //  JSONRPCError.swift
 //  JSONRPCKit
 //
-//  Created by Shinichiro Oba on 2015/11/09.
-//  Copyright © 2015年 Shinichiro Oba. All rights reserved.
+//  Created by Petr Shtuka 05/10/2023.
 //
-
-// import Foundation
-
-// public enum JSONRPCError: Error {
-//     case responseError(code: Int, message: String, data: Any?)
-//     case responseNotFound(requestId: Id?, object: Any)
-//     case resultObjectParseError(Error)
-//     case errorObjectParseError(Error)
-//     case sessionExpired
-//     case unsupportedVersion(String?)
-//     case unexpectedTypeObject(Any)
-//     case missingBothResultAndError(Any)
-//     case nonArrayResponse(Any)
-
-//     init?(response: [String: Any]) {
-//     if let errorObject = response["error"] as? [String: Any],
-//        let code = errorObject["code"] as? Int,
-//        let message = errorObject["message"] as? String {
-//         if code == 100 && message == "Odoo Session Expired" {
-//             self = .sessionExpired
-//         } else {
-//             let data = errorObject["data"]
-//             self = .responseError(code: code, message: message, data: data)
-//         }
-//     } else {
-//         // Ensure "id" is of type Id or is null
-//         if let requestId = response["id"] as? Id? {
-//             self = .responseNotFound(requestId: requestId, object: response)
-//         } else {
-//             return nil
-//         }
-//     }
-//     }
-// }
-
-// public class JSONRPCResponseSerializer {
-//     public func serializeResponse(_ response: [String: Any]) -> Result<Any, JSONRPCError> {
-//         if let error = JSONRPCError(response: response) {
-//             return .failure(error)
-//         }
-//         // Parse and return the result object here, you may need to customize this based on your response structure
-//         guard let result = response["result"] else {
-//             return .failure(.missingBothResultAndError(response))
-//         }
-//         return .success(result)
-//     }
-// }
-
-// extension JSONRPCError: Equatable {
-//     static func == (lhs: JSONRPCError, rhs: JSONRPCError) -> Bool {
-//         switch (lhs, rhs) {
-//         case (.sessionExpired, .sessionExpired):
-//             return true
-//         default:
-//             return false
-//         }
-//     }
-// }
 
 import Foundation
 
@@ -76,25 +17,53 @@ public enum JSONRPCError: Error {
     case unexpectedTypeObject(Any)
     case missingBothResultAndError(Any)
     case nonArrayResponse(Any)
+    case connectionError(String)
+	case networkConnectionLost
+
+    public var description: String {
+        switch self {
+        case .responseError(let code, let message, _):
+            return "Response error (\(code)): \(message)"
+        case .responseNotFound(let requestId, _):
+            return "Response not found for request ID: \(String(describing: requestId))"
+        case .resultObjectParseError:
+            return "Failed to parse result object."
+        case .errorObjectParseError:
+            return "Failed to parse error object."
+        case .sessionExpired:
+            return "Session has expired."
+        case .unsupportedVersion(let version):
+            if let ver = version {
+                return "Unsupported version: \(ver)"
+            }
+            return "Unsupported version."
+        case .unexpectedTypeObject:
+            return "Unexpected type for object."
+        case .missingBothResultAndError:
+            return "Both result and error are missing in the response."
+        case .nonArrayResponse:
+            return "Expected an array in the response."
+        case .connectionError(let message):
+            return message
+ 	case .networkConnectionLost:
+        return "The network connection was lost."
+        }
+    }
 
     init?(response: [String: Any]) {
         if let errorObject = response["error"] as? [String: Any],
            let code = errorObject["code"] as? Int,
            let message = errorObject["message"] as? String {
             if code == 100 && message == "Odoo Session Expired" {
-                print("Session expired.")
                 self = .sessionExpired
             } else {
                 let data = errorObject["data"]
-                print("Response error: \(code) - \(message)")
                 self = .responseError(code: code, message: message, data: data)
             }
         } else {
             if let requestId = response["id"] as? Id? {
-                print("Response not found for request ID: \(String(describing: requestId))")
                 self = .responseNotFound(requestId: requestId, object: response)
             } else {
-                print("Error: unexpected response format.")
                 return nil
             }
         }
@@ -104,28 +73,24 @@ public enum JSONRPCError: Error {
 public class JSONRPCResponseSerializer {
     public func serializeResponse(_ response: [String: Any]) -> Result<Any, JSONRPCError> {
         if let error = JSONRPCError(response: response) {
-            print("Error occurred: \(error)")
             return .failure(error)
         }
         guard let result = response["result"] else {
-            print("Error: result and error both missing from response.")
             return .failure(.missingBothResultAndError(response))
         }
-        print("Successfully serialized the response.")
         return .success(result)
     }
 }
 
 extension JSONRPCError: Equatable {
-    static func == (lhs: JSONRPCError, rhs: JSONRPCError) -> Bool {
+    public static func == (lhs: JSONRPCError, rhs: JSONRPCError) -> Bool {
         switch (lhs, rhs) {
         case (.sessionExpired, .sessionExpired):
-            print("Both errors are session expired errors.")
             return true
         default:
-            print("Errors are not equal.")
             return false
         }
     }
 }
+
 
